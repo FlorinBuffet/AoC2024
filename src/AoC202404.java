bcda713dea57acac23fab8b8dcee6466268a1489
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-04 07:36:02 +0100

    AoC 2024, Day 4 Finished

--- src/AoC202404.java
+++ src/AoC202404.java
@@ -37,11 +37,30 @@ public class AoC202404 {
             found += StringUtils.countMatches(thisLine, "SAMX");
         }
 
+        //find vertical matches
+        for(int j = 0; j<playField.length; j++){
+            String thisLine = "";
+            for(int i = 0; i<playField.length; i++){
+                thisLine += playField[i][j];
+            }
+            found += StringUtils.countMatches(thisLine, "XMAS");
+            found += StringUtils.countMatches(thisLine, "SAMX");
+        }
 
-        char lastChar = ' ';
-        for (int i = 0; i < playField.length; i++) {
-            for (int j = 0; j < playField[i].length; j++) {
-
+        for(int i = 3; i<playField.length; i++){
+            for(int j = 3; j<playField.length;j++){
+                if (playField[i-3][j-3]=='X' && playField[i-2][j-2]=='M' && playField[i-1][j-1]=='A'&&playField[i][j]=='S'){
+                    found++;
+                }else if (playField[i-3][j-3]=='S' && playField[i-2][j-2]=='A' && playField[i-1][j-1]=='M'&&playField[i][j]=='X'){
+                    found++;
+                }
+            }
+            for (int j = playField.length-4; j>=0; j--){
+                if (playField[i-3][j+3]=='X' && playField[i-2][j+2]=='M' && playField[i-1][j+1]=='A'&&playField[i][j]=='S'){
+                    found++;
+                }else if (playField[i-3][j+3]=='S' && playField[i-2][j+2]=='A' && playField[i-1][j+1]=='M'&&playField[i][j]=='X'){
+                    found++;
+                }
             }
         }
         
@@ -50,7 +69,26 @@ public class AoC202404 {
 
     public static int partTwo(String path) throws FileNotFoundException {
         char[][] playField = readFile(path);
+        int found = 0;
 
-        return 0;
+        for(int i = 1; i<playField.length-1; i++){
+            for(int j = 1; j<playField.length-1;j++){
+                if (playField[i-1][j-1]=='M'&&playField[i][j]=='A'&&playField[i+1][j+1]=='S'){
+                    if (playField[i-1][j+1]=='M'&&playField[i+1][j-1]=='S'){
+                        found++;
+                    }else if (playField[i-1][j+1]=='S'&&playField[i+1][j-1]=='M'){
+                        found++;
+                    }
+                }else if (playField[i-1][j-1]=='S'&&playField[i][j]=='A'&&playField[i+1][j+1]=='M'){
+                    if (playField[i-1][j+1]=='M'&&playField[i+1][j-1]=='S'){
+                        found++;
+                    }else if (playField[i-1][j+1]=='S'&&playField[i+1][j-1]=='M'){
+                        found++;
+                    }
+                }
+            }
+        }
+
+        return found;
     }
 }
