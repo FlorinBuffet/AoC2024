efefe0023ad81488977341a1a7e937ed281d68d9
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-04 08:19:09 +0100

    AoC 2024, Day 3 CleanCode

--- src/AoC202403.java
+++ src/AoC202403.java
@@ -5,8 +5,27 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class solves AdventofCode 2024, Day 3.
+ *
+ * @author Florin Buffet
+ * @version V1.1
+ */
 public class AoC202403 {
 
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private AoC202403() {
+    }
+
+    /**
+     * Reads the input file and returns the data for the day's challenge.
+     *
+     * @param path path to the input file
+     * @return the data as a list of strings
+     * @throws FileNotFoundException
+     */
     private static ArrayList<String> readFile(String path) throws FileNotFoundException {
         ArrayList<String> inputDataOne = new ArrayList<>();
 
@@ -27,6 +46,11 @@ public class AoC202403 {
         return inputDataOne;
     }
 
+    /**
+     * @param path path to the input file
+     * @return the result for the first part of the challenge
+     * @throws FileNotFoundException
+     */
     public static int partOne(String path) throws FileNotFoundException {
         ArrayList<String> inputData = readFile(path);
 
@@ -34,14 +58,19 @@ public class AoC202403 {
         while (!inputData.isEmpty()) {
             String currentSample = inputData.removeFirst();
             if (currentSample.contains("mul")) {
-                int number1 = Integer.parseInt(currentSample.substring(4, currentSample.indexOf(",")));
-                int number2 = Integer.parseInt(currentSample.substring(currentSample.indexOf(",") + 1, currentSample.length() - 1));
+                int number1 = Integer.parseInt(currentSample.substring(4, currentSample.indexOf(',')));
+                int number2 = Integer.parseInt(currentSample.substring(currentSample.indexOf(',') + 1, currentSample.length() - 1));
                 result += number1 * number2;
             }
         }
         return result;
     }
 
+    /**
+     * @param path path to the input file
+     * @return the result for the second part of the challenge
+     * @throws FileNotFoundException
+     */
     public static int partTwo(String path) throws FileNotFoundException {
         ArrayList<String> inputData = readFile(path);
 
@@ -54,8 +83,8 @@ public class AoC202403 {
             } else if ("don't()".equals(currentSample)) {
                 isDo = false;
             } else if (currentSample.contains("mul") && isDo) {
-                int number1 = Integer.parseInt(currentSample.substring(4, currentSample.indexOf(",")));
-                int number2 = Integer.parseInt(currentSample.substring(currentSample.indexOf(",") + 1, currentSample.length() - 1));
+                int number1 = Integer.parseInt(currentSample.substring(4, currentSample.indexOf(',')));
+                int number2 = Integer.parseInt(currentSample.substring(currentSample.indexOf(',') + 1, currentSample.length() - 1));
                 result += number1 * number2;
             }
         }
