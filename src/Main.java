c66a7d8eecff71cad33d388f6aa10199889d0c83
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-04 08:20:44 +0100

    AoC 2024 Main CleanCode

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 import java.io.FileNotFoundException;
 
+/**
+ * This class solves AdventofCode 2024
+ *
+ * @author Florin Buffet
+ * @version V1.0
+ */
 public class Main {
+    private Main() {
+    }
+
+    /**
+     * Main method to run the challenges.
+     *
+     * @param args
+     * @throws FileNotFoundException
+     */
+    @SuppressWarnings("DuplicateStringLiteralInspection")
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("2024 Day 01, Part 1: "+AoC202401.partOne("data/AoC_2024_01.txt"));
         System.out.println("2024 Day 01, Part 2: "+AoC202401.partTwo("data/AoC_2024_01.txt"));
