b0f213204e12df8c1ab0d605f98086bf9359981c
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-04 08:06:27 +0100

    AoC 2024, Day 1 CleanCode

--- src/AoC202401.java
+++ src/AoC202401.java
@@ -5,22 +5,39 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class solves AdventofCode 2024, Day 1.
+ *
+ * @author Florin Buffet
+ * @version V1.1
+ */
 public class AoC202401 {
 
-    private static List<Integer>[] readFile(String path) throws FileNotFoundException {
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private AoC202401() {
+    }
+
+    private static List[] readFile(String path) throws FileNotFoundException {
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
         File file = new File(path);
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             String data = scanner.nextLine();
-            left.add(Integer.parseInt(data.substring(0, data.indexOf(" "))));
-            right.add(Integer.parseInt(data.substring(data.indexOf(" ") + 1).trim()));
+            left.add(Integer.parseInt(data.substring(0, data.indexOf(' '))));
+            right.add(Integer.parseInt(data.substring(data.indexOf(' ') + 1).trim()));
         }
         scanner.close();
         return new List[]{left, right};
     }
 
+    /**
+     * @param path path to the input file
+     * @return the result for the first part of the challenge
+     * @throws FileNotFoundException
+     */
     public static int partOne(String path) throws FileNotFoundException {
         List<Integer>[] lists = readFile(path);
         List<Integer> left = lists[0];
@@ -35,6 +52,11 @@ public class AoC202401 {
         return returnVal;
     }
 
+    /**
+     * @param path path to the input file
+     * @return the result for the second part of the challenge
+     * @throws FileNotFoundException
+     */
     public static long partTwo(String path) throws FileNotFoundException {
         List<Integer>[] lists = readFile(path);
         List<Integer> left = lists[0];
