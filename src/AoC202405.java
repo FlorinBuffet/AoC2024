97d961d998f674686028f77a5830d8e18fef0b09
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-05 10:03:52 +0100

    AoC 2024, Day 5, Part 1 CleanCode

--- src/AoC202405.java
+++ src/AoC202405.java
@@ -1,29 +1,50 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * This class solves AdventofCode 2024, Day 5.
+ *
+ * @author Florin Buffet
+ * @version V1.0
+ */
 public class AoC202405 {
 
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private AoC202405() {
+    }
+
+    /**
+     * Reads the input file and returns the data for the day's challenge.
+     *
+     * @param path path to the input file
+     * @return the data as a list of chars
+     * @throws FileNotFoundException
+     */
     private static Object[] readFile(String path) throws FileNotFoundException {
         File file = new File(path);
         Scanner scanner = new Scanner(file);
 
-        Map<Integer,Integer> orderingRules = new HashMap<>();
-        List<List<Integer>> pageNumbers = new ArrayList<>();
+        Collection<int[]> orderingRules = new ArrayList<>();
+        Collection<List<Integer>> pageNumbers = new ArrayList<>();
 
-        boolean emptyLineFound = false;
+        boolean emptyLineNotFound = true;
 
         while (scanner.hasNextLine()) {
             String data = scanner.nextLine();
             if ("".equals(data)) {
-                emptyLineFound = true;
-            }else if(!emptyLineFound){
+                emptyLineNotFound = false;
+            }else if(emptyLineNotFound){
                 String[] split = data.split("\\|");
-                orderingRules.put(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+                int[] tmpArray = new int[2];
+                tmpArray[0] = Integer.parseInt(split[0]);
+                tmpArray[1] = Integer.parseInt(split[1]);
+                orderingRules.add(tmpArray);
             }else{
                 String[] split = data.split(",");
                 List<Integer> currentPageNumbers = new ArrayList<>();
@@ -40,22 +61,19 @@ public class AoC202405 {
         return objects;
     }
 
+    /**
+     * @param path path to the input file
+     * @return the result for the first part of the challenge
+     * @throws FileNotFoundException
+     */
     public static int partOne(String path) throws FileNotFoundException {
         Object[] objects = readFile(path);
-        Map<Integer,Integer> orderingRules = (Map<Integer, Integer>) objects[0];
-        List<List<Integer>> pageNumbers = (List<List<Integer>>) objects[1];
+        Iterable<int[]> orderingRules = (Iterable<int[]>) objects[0];
+        Iterable<List<Integer>> pageNumbers = (Iterable<List<Integer>>) objects[1];
         int sum = 0;
 
         for (List<Integer> currentPageNumbers : pageNumbers) {
-            boolean validResult = true;
-            for (Map.Entry<Integer, Integer> entry : orderingRules.entrySet()) {
-                if (currentPageNumbers.contains(entry.getKey()) && currentPageNumbers.contains(entry.getValue())) {
-                    if (currentPageNumbers.indexOf(entry.getKey()) > currentPageNumbers.indexOf(entry.getValue())) {
-                        validResult = false;
-                    }
-                }
-            }
-            if (validResult) {
+            if (arePagesOrdered(currentPageNumbers, orderingRules)) {
                 sum += currentPageNumbers.get((currentPageNumbers.size()-1)/2);
             }
         }
@@ -63,11 +81,56 @@ public class AoC202405 {
         return sum;
     }
 
+    /**
+     * @param path path to the input file
+     * @return the result for the second part of the challenge
+     * @throws FileNotFoundException
+     */
     public static int partTwo(String path) throws FileNotFoundException {
         Object[] objects = readFile(path);
-        Map<Integer,Integer> orderingRules = (Map<Integer, Integer>) objects[0];
-        List<List<Integer>> pageNumbers = (List<List<Integer>>) objects[1];
+        Iterable<int[]> orderingRules = (Iterable<int[]>) objects[0];
+        Iterable<List<Integer>> pageNumbers = (Iterable<List<Integer>>) objects[1];
+
+        for (List<Integer> currentPageNumbers : pageNumbers) {
+
+        }
 
         return 0;
     }
+
+    /**
+     * Checks if the given pages follow the rules.
+     *
+     * @param pageNumbers The list of page numbers in the update.
+     * @param orderingRules  The list of rules represented as int arrays where rules[i][0] must be before rules[i][1].
+     * @return True if the update is correctly ordered according to the rules, false otherwise.
+     */
+    private static boolean arePagesOrdered(List<Integer> pageNumbers, Iterable<int[]> orderingRules) {
+        boolean ordered = true;
+        for (int[] rule : orderingRules) {
+
+            //skips rules that contain pages that are no in the current update
+            if (pageNumbers.contains(rule[0]) && pageNumbers.contains(rule[1])) {
+
+                //if the first page is after the second page, the update is not ordered
+                if (pageNumbers.indexOf(rule[0]) > pageNumbers.indexOf(rule[1])) {
+                    ordered = false;
+                }
+            }
+        }
+        return ordered;
+    }
+
+    /**
+     * Sorts the given pages according to the rules.
+     *
+     * @param pageNumbers The list of page numbers in the update.
+     * @param orderingRules  The list of rules represented as int arrays where rules[i][0] must be before rules[i][1].
+     * @return True if the update is correctly ordered according to the rules, false otherwise.
+     */
+    private static boolean sortPages(List<Integer> pageNumbers, Iterable<int[]> orderingRules) {
+
+        return true;
+    }
 }
+
---
+++ src/AoC202405_comparator.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+import java.util.Comparator;
+
+public class AoC202405_comparator implements Comparator<Integer>, Serializable {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+
+    }
+}
