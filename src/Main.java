3b5f1b1fbc3f70e8ed608a7d223e453c9940e66a
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-12 15:30:11 +0100

    Main.java updated CleanCode

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
 
 /**
  * This class solves AdventofCode 2024
  *
  * @author Florin Buffet
- * @version V1.0
+ * @version V1.1
  */
 public class Main {
     private Main() {
@@ -13,12 +14,31 @@ public class Main {
     /**
      * Main method to run the challenges.
      *
-     * @param args
-     * @throws FileNotFoundException
+     * @param args command line arguments
+     * @throws FileNotFoundException if the specified file is not found
      */
-    @SuppressWarnings("DuplicateStringLiteralInspection")
+    @SuppressWarnings("MagicNumber")
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("2024 Day 11, Part 1: " + AoC202411.partOne("data/AoC_2024_11.txt"));
-        System.out.println("2024 Day 11, Part 2: " + AoC202411.partTwo("data/AoC_2024_11.txt"));
+
+        // Please change the year and day to the desired challenge
+        int year = 2024;
+        int day = 12;
+
+        String filePath = "data/AoC_" + year + "_" + String.format("%02d", day) + ".txt";
+        //noinspection StringConcatenationMissingWhitespace
+        String className = "AoC" + year + String.format("%02d", day);
+
+        //noinspection OverlyBroadCatchBlock
+        try {
+            Class<?> usedClass = Class.forName(className);
+            Method partOneMethod = usedClass.getMethod("partOne", String.class);
+            Method partTwoMethod = usedClass.getMethod("partTwo", String.class);
+
+            System.out.println("2024 Day " + day + ", Part 1: " + partOneMethod.invoke(null, filePath));
+            System.out.println("2024 Day " + day + ", Part 2: " + partTwoMethod.invoke(null, filePath));
+        } catch (Exception e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 }
