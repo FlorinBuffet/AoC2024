b10b65e37007435fa8df71ab17cecd5f0a5c5865
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-19 09:34:35 +0100

    Updated AoCDownloader and made appropriate changes to Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import utility.AoCDownloader;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.util.Properties;
+import java.net.URISyntaxException;
 
 /**
  * This class solves AdventofCode 2024
@@ -12,8 +11,6 @@ import java.util.Properties;
  * @version V1.2
  */
 public class Main {
-    private static final String PROPERTY_SESSION_TOKEN = "SESSION_TOKEN";
-
     private Main() {
     }
 
@@ -21,50 +18,37 @@ public class Main {
      * Main method to run the challenges.
      *
      * @param args command line arguments
+     * @throws IOException if the file is not found
+     * @throws URISyntaxException if the URI is invalid
      */
     @SuppressWarnings("MagicNumber")
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
         // Please change the year and day to the desired challenge
         int year = 2024;
-        int day = 4;
-        String sessionToken = "";
+        int day = 19;
 
-        if (!AoCDownloader.doesInputExist(year, day)) {
-            System.out.println("Downloading input file for day " + day + " of year " + year);
-            Properties prop = new Properties();
-            InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("AoC.properties");
-            prop.load(stream);
-            if ("true".equals(prop.getProperty("ALLOW_DOWNLOADING_OF_INPUT_FILES"))) {
-                if (prop.getProperty(PROPERTY_SESSION_TOKEN) == null || prop.getProperty(PROPERTY_SESSION_TOKEN).isEmpty()) {
-                    System.out.println("Please provide a session token in the AoC.properties file.");
-                    System.exit(1);
-                } else {
-                    sessionToken = prop.getProperty(PROPERTY_SESSION_TOKEN);
-                    AoCDownloader.downloadInputToFile(year, day, sessionToken);
-                }
-            } else {
-                System.out.println("Please provide the input file in the data folder or allow downloading in the AoC.properties file.");
-                System.exit(1);
-            }
-        }
-
-        String filePath = "data/AoC_" + year + "_" + String.format("%02d", day) + ".txt";
-        //noinspection StringConcatenationMissingWhitespace
-        String className = "AoC" + year + String.format("%02d", day);
-
-        //noinspection OverlyBroadCatchBlock
-        try {
+        if (AoCDownloader.doesFileExistOrIsDownloaded(year, day, true)) {
+            System.out.println("Calculating the result for the challenge...");
+            String filePath = "data/AoC_" + year + "_" + String.format("%02d", day) + ".txt";
             //noinspection StringConcatenationMissingWhitespace
-            Class<?> usedClass = Class.forName("year" + year + "." + className);
-            Method partOneMethod = usedClass.getMethod("partOne", String.class);
-            Method partTwoMethod = usedClass.getMethod("partTwo", String.class);
+            String className = "AoC" + year + String.format("%02d", day);
 
-            System.out.println("2024 Day " + day + ", Part 1: " + partOneMethod.invoke(null, filePath));
-            System.out.println("2024 Day " + day + ", Part 2: " + partTwoMethod.invoke(null, filePath));
-        } catch (Exception e) {
-            //noinspection CallToPrintStackTrace
-            e.printStackTrace();
+            //noinspection OverlyBroadCatchBlock
+            try {
+                //noinspection StringConcatenationMissingWhitespace
+                Class<?> usedClass = Class.forName("year" + year + "." + className);
+                Method partOneMethod = usedClass.getMethod("partOne", String.class);
+                Method partTwoMethod = usedClass.getMethod("partTwo", String.class);
+
+                System.out.println(year + " Day " + day + ", Part 1: " + partOneMethod.invoke(null, filePath));
+                System.out.println(year + " Day " + day + ", Part 2: " + partTwoMethod.invoke(null, filePath));
+            } catch (Exception e) {
+                //noinspection CallToPrintStackTrace
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("The software exits now.");
         }
     }
 }
--- src/utility/AoCDownloader.java
+++ src/utility/AoCDownloader.java
@@ -1,20 +1,28 @@
 package utility;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Properties;
 import java.util.Scanner;
 
 /**
  * This class automatically downloads the input for the Advent of Code challenges.
  *
  * @author Florin Buffet
- * @version V1.0
+ * @version V1.1
  */
 public class AoCDownloader {
+
+    private static final String PROPERTY_SESSION_TOKEN = "SESSION_TOKEN";
+    private static final String PROPERTY_ALLOW_DOWNLOADING = "ALLOW_DOWNLOADING_OF_INPUT_FILES";
+    private static final int HTTP_STATUS_OK = 200;
+
     /**
      * Private constructor to prevent instantiation.
      */
@@ -24,32 +32,31 @@ public class AoCDownloader {
     /**
      * Downloads the input for the specified day and year.
      *
-     * @param day  the day of the challenge
+     * @param day          the day of the challenge
      * @param sessionToken the session token from the website
-     * @param year the year of the challenge
+     * @param year         the year of the challenge
      * @return the input as a string
      */
-    public static String downloadInput(int year, int day, String sessionToken) {
+    @SuppressWarnings({"OverlyBroadThrowsClause", "MethodWithMultipleReturnPoints"})
+    private static String getInputFromAPI(int year, int day, String sessionToken) throws URISyntaxException, IOException {
         String urlString = "https://adventofcode.com/" + year + "/day/" + day + "/input";
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("Cookie", "session=" + sessionToken);
-            connection.setRequestMethod("GET");
+        URL url = new URI(urlString).toURL();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Cookie", "session=" + sessionToken);
+        connection.setRequestMethod("GET");
 
-            if (connection.getResponseCode() == 200) {
-                Scanner scanner = new Scanner(connection.getInputStream());
-                StringBuilder response = new StringBuilder();
-                while (scanner.hasNextLine()) {
-                    response.append(scanner.nextLine()).append("\n");
-                }
-                scanner.close();
-                return response.toString();
+        if (connection.getResponseCode() == HTTP_STATUS_OK) {
+            Scanner scanner = new Scanner(connection.getInputStream());
+            StringBuilder response = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                response.append(scanner.nextLine()).append("\n");
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            scanner.close();
+            return response.toString();
+        } else {
+            System.out.println("Error: " + connection.getResponseCode());
+            return connection.getResponseMessage();
         }
-        return null;
     }
 
     /**
@@ -59,25 +66,73 @@ public class AoCDownloader {
      * @param year the year of the challenge
      * @return the input as a string
      */
-    @SuppressWarnings("StringConcatenationMissingWhitespace")
-    public static boolean doesInputExist(int year, int day) {
+    private static boolean doesInputExist(int year, int day) {
         String fileName = "data/AoC_" + year + "_" + String.format("%02d", day) + ".txt";
         return Files.exists(Path.of(fileName));
     }
 
-    public static boolean downloadInputToFile(int year, int day, String sessionToken) {
-        if (!doesInputExist(year, day)) {
-            String input = downloadInput(year, day, sessionToken);
-            if (input != null) {
-                String fileName = "data/AoC_" + year + "_" + String.format("%02d", day) + ".txt";
-                try {
-                    Files.writeString(Path.of(fileName), input);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return false;
+    /**
+     * Downloads the input for the specified day and year to a file.
+     * If the input is successfully retrieved from the API, it writes the input to a file in the data directory.
+     *
+     * @param year         the year of the challenge
+     * @param day          the day of the challenge
+     * @param sessionToken the session token from the website
+     * @return true if the input file exists after the download attempt, false otherwise
+     * @throws IOException if an I/O error occurs during file writing
+     */
+    @SuppressWarnings({"BooleanMethodNameMustStartWithQuestion", "MethodWithMultipleReturnPoints"})
+    private static boolean downloadInputToFile(int year, int day, String sessionToken) throws IOException, URISyntaxException {
+        String input = getInputFromAPI(year, day, sessionToken);
+        if ("Internal Server Error".equals(input)) {
+            System.out.println("Please check the session token in the AoC.properties file.");
+            return false;
+        } else {
+            String fileName = "data/AoC_" + year + "_" + String.format("%02d", day) + ".txt";
+            Files.writeString(Path.of(fileName), input);
+            return doesInputExist(year, day);
+        }
+    }
+
+    /**
+     * Checks if the input file for the specified day and year exists or downloads it if allowed.
+     * If the input file does not exist, it attempts to download it using the session token from the properties file.
+     *
+     * @param year          the year of the challenge
+     * @param day           the day of the challenge
+     * @param printMessages whether to print messages during the process
+     * @return true if the input file exists or is successfully downloaded, false otherwise
+     * @throws IOException        if an I/O error occurs
+     * @throws URISyntaxException if the URI is invalid
+     */
+    public static boolean doesFileExistOrIsDownloaded(int year, int day, boolean printMessages) throws IOException, URISyntaxException {
+        boolean returnBoolean;
+        if (doesInputExist(year, day))
+            returnBoolean = true;
+        else {
+            if (printMessages)
+                System.out.println("Downloading input file for day " + day + " of year " + year);
+
+            // Load the properties file
+            Properties prop = new Properties();
+            InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("AoC.properties");
+            prop.load(stream);
+
+            if (!"true".equals(prop.getProperty(PROPERTY_ALLOW_DOWNLOADING))) {
+                System.out.println("Please provide the input file in the data folder or allow downloading in the AoC.properties file.");
+                returnBoolean = false;
+            } else if (prop.getProperty(PROPERTY_SESSION_TOKEN) == null || prop.getProperty(PROPERTY_SESSION_TOKEN).isEmpty()) {
+                System.out.println("Please provide a session token in the AoC.properties file.");
+                returnBoolean = false;
+            } else {
+                String sessionToken = prop.getProperty(PROPERTY_SESSION_TOKEN);
+                returnBoolean = downloadInputToFile(year, day, sessionToken);
+            }
+
+            if (printMessages && returnBoolean) {
+                System.out.println("The input file was successfully downloaded.");
             }
         }
-        return doesInputExist(year, day);
+        return returnBoolean;
     }
 }
