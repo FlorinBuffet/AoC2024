8bf6ebac51cc24cef8a9578ba67c54398957f911
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-19 12:28:51 +0100

    AoC 2024, Day 10, cleanup, created Test and used InputParser and BooleanGridHelper

--- src/utility/InputParser.java
+++ src/utility/InputParser.java
@@ -1,6 +1,7 @@
 package utility;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  * This class implements some input parser functions.
@@ -29,6 +30,24 @@ public class InputParser {
         matrix[0] = firstLine.toCharArray();
         for (int i = 1; i < matrix.length; i++) {
             matrix[i] = scan.nextLine().toCharArray();
+        }
+        return matrix;
+    }
+
+    /**
+     * Parses a square matrix of integers from the given Scanner.
+     * The matrix is assumed to be square, with each line having the same length.
+     *
+     * @param scan      the Scanner to read the input from
+     * @param delimiter the delimiter to use when parsing the integers
+     * @return a 2D array of integers representing the square matrix
+     */
+    public static int[][] parseIntegerSquareMatrix(Scanner scan, String delimiter) {
+        String firstLine = scan.nextLine();
+        int[][] matrix = new int[firstLine.length()][firstLine.length()];
+        matrix[0] = Stream.of(firstLine.split(delimiter)).mapToInt(Integer::parseInt).toArray();
+        for (int i = 1; i < matrix.length; i++) {
+            matrix[i] = Stream.of(scan.nextLine().split(delimiter)).mapToInt(Integer::parseInt).toArray();
         }
         return matrix;
     }
--- src/year2024/AoC202410.java
+++ src/year2024/AoC202410.java
@@ -1,12 +1,11 @@
 package year2024;
 
+import utility.BooleanGridHelper;
+import utility.InputParser;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.stream.Stream;
-
-//TODO: cleanup class
-//TODO: Use BooleanGridHelper class
 
 /**
  * This class solves AdventofCode 2024, Day 10.
@@ -27,30 +26,23 @@ public class AoC202410 {
      *
      * @param path path to the input file
      * @return the data as a list of chars
-     * @throws FileNotFoundException
+     * @throws FileNotFoundException if the file is not found
      */
     private static int[][] readFile(String path) throws FileNotFoundException {
         File file = new File(path);
         Scanner scanner = new Scanner(file);
 
-        int[][] data = new int[1][1];
-        int currentLine = 0;
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (data.length == 1) {
-                data = new int[line.length()][line.length()];
-            }
-            data[currentLine] = Stream.of(line.split("")).mapToInt(Integer::parseInt).toArray();
-            currentLine++;
-        }
+        int[][] data = InputParser.parseIntegerSquareMatrix(scanner, "");
         scanner.close();
         return data;
     }
 
     /**
+     * Calculates the result for the first part of the challenge.
+     *
      * @param path path to the input file
-     * @return the result for the first part of the challenge
-     * @throws FileNotFoundException
+     * @return the result as an integer.
+     * @throws FileNotFoundException if the file is not found
      */
     public static int partOne(String path) throws FileNotFoundException {
         int[][] data = readFile(path);
@@ -60,7 +52,7 @@ public class AoC202410 {
                 if (data[x][y] == 0) {
                     boolean[][] accessed = new boolean[data.length][data.length];
                     calculateAccessedNine(x, y, data, accessed);
-                    sum += countTruesInBooleanArray(accessed);
+                    sum += BooleanGridHelper.countTrue(accessed);
                 }
             }
         }
@@ -68,12 +60,16 @@ public class AoC202410 {
     }
 
     /**
-     * @param x        x coordinate
-     * @param y        y coordinate
-     * @param data     the map including height information
-     * @param accessed the map of fields that are the highest height that have been accessed
+     * Recursively marks cells with a value of 9 as accessed starting from the given coordinates.
+     * The method checks the adjacent cells (up, down, left, right) and continues the search if the adjacent cell
+     * has a value that is one greater than the current cell.
+     *
+     * @param x the x-coordinate of the starting cell
+     * @param y the y-coordinate of the starting cell
+     * @param data the 2D array representing the grid with height information
+     * @param accessed the 2D boolean array to mark the accessed cells
      */
-    public static void calculateAccessedNine(int x, int y, int[][] data, boolean[][] accessed) {
+    private static void calculateAccessedNine(int x, int y, int[][] data, boolean[][] accessed) {
         if (x < data.length - 1 && data[x][y] + 1 == data[x + 1][y]) {
             calculateAccessedNine(x + 1, y, data, accessed);
         }
@@ -92,27 +88,11 @@ public class AoC202410 {
     }
 
     /**
-     * Function that counts the number of true values in a boolean array.
+     * Calculates the result for the second part of the challenge.
      *
-     * @param array the boolean array
-     * @return the number of true values in the array
-     */
-    public static int countTruesInBooleanArray(boolean[][] array) {
-        int sum = 0;
-        for (boolean[] booleans : array) {
-            for (boolean aBoolean : booleans) {
-                if (aBoolean) {
-                    sum++;
-                }
-            }
-        }
-        return sum;
-    }
-
-    /**
      * @param path path to the input file
-     * @return the result for the second part of the challenge
-     * @throws FileNotFoundException
+     * @return the result as an integer.
+     * @throws FileNotFoundException if the file is not found
      */
     public static int partTwo(String path) throws FileNotFoundException {
         int[][] data = readFile(path);
@@ -128,12 +108,16 @@ public class AoC202410 {
     }
 
     /**
-     * @param x    x coordinate
-     * @param y    y coordinate
-     * @param data the map including height information
-     * @return the number of routes to the highest height
+     * Recursively calculates the number of routes to cells with a value of 9 starting from the given coordinates.
+     * The method checks the adjacent cells (up, down, left, right) and continues the search if the adjacent cell
+     * has a value that is one greater than the current cell.
+     *
+     * @param x    the x-coordinate of the starting cell
+     * @param y    the y-coordinate of the starting cell
+     * @param data the 2D array representing the grid with height information
+     * @return the number of routes to cells with a value of 9
      */
-    public static int calculateRoutesToNines(int x, int y, int[][] data) {
+    private static int calculateRoutesToNines(int x, int y, int[][] data) {
         int sum = 0;
         if (x < data.length - 1 && data[x][y] + 1 == data[x + 1][y]) {
             sum += calculateRoutesToNines(x + 1, y, data);
