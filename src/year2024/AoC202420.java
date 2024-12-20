42632737758a5c0cafb3d4cdafe5ee8486cb691b
Florin Buffet <florin.buffet@buffetitservices.ch>
2024-12-20 22:22:37 +0100

    AoC 2024, Day 20, optimized

--- src/year2024/AoC202420.java
+++ src/year2024/AoC202420.java
@@ -7,18 +7,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-//TODO: improve implementation
-
 /**
  * This class solves AdventofCode 2024, Day Template.
  *
  * @author Florin Buffet
- * @version V1.1
+ * @version V2.0
  */
 @SuppressWarnings("FeatureEnvy")
 public class AoC202420 {
 
     private static final int GRID_SIZE_LIMIT_FOR_SAMPLES = 20;
+    private static final int ROUNDS_PART_TWO = 20;
+    private static final int ROUNDS_PART_ONE = 2;
 
     /**
      * Private constructor to prevent instantiation.
@@ -63,13 +63,25 @@ public class AoC202420 {
      */
     public static int partOne(String path) throws FileNotFoundException {
         NodeGrid grid = readFile(path);
+        grid.calculateDistances();
+        return calculateNumberOfCheats(grid, ROUNDS_PART_ONE);
+    }
 
-        int shortestPath = grid.getDistanceFromStartToEnd();
+    /**
+     * Calculates the number of cheats that save picoseconds within the grid.
+     *
+     * @param grid   the NodeGrid object representing the grid
+     * @param rounds the number of rounds to create cheats
+     * @return the total number of saving cheats
+     */
+    private static int calculateNumberOfCheats(NodeGrid grid, int rounds) {
         int shorterPath = 0;
 
-        for (int i = 1; i < grid.getSize() - 1; i++) {
-            for (int j = 1; j < grid.getSize() - 1; j++) {
-                shorterPath += checkIfCheatOver100(grid, i, j, shortestPath) ? 1 : 0;
+        for (int i = 1; i <= grid.getSize() - 2; i++) {
+            for (int j = 1; j <= grid.getSize() - 2; j++) {
+                if (grid.doesNodeExist(i, j)) {
+                    shorterPath += createCheats(grid, i, j, rounds);
+                }
             }
         }
 
@@ -77,26 +89,34 @@ public class AoC202420 {
     }
 
     /**
-     * Checks if adding a node at the specified coordinates saves more than 100 picoseconds.
+     * Creates cheats within a specified maximum distance from the start node and checks if they save picoseconds.
      *
-     * @param grid         the NodeGrid object representing the grid
-     * @param x            the x-coordinate of the node
-     * @param y            the y-coordinate of the node
-     * @param shortestPath the current shortest path length
-     * @return true if adding the node saves more than 100 picoseconds, false otherwise
+     * @param grid        the NodeGrid object representing the grid
+     * @param startX      the x-coordinate of the start node
+     * @param startY      the y-coordinate of the start node
+     * @param maxDistance the maximum distance to create cheats
+     * @return the number of saving cheats
      */
-    private static boolean checkIfCheatOver100(NodeGrid grid, int x, int y, int shortestPath) {
-        boolean isSaving = false;
-        if (!grid.doesNodeExist(x, y)) {
-            grid.addNode(x, y);
-            int thisPath = grid.getDistanceFromStartToEnd();
-            int picosecondsSaved = grid.getSize() <= GRID_SIZE_LIMIT_FOR_SAMPLES ? 10 : 100;
-            if (thisPath <= shortestPath - picosecondsSaved) {
-                isSaving = true;
-            }
-            grid.deleteNode(x, y);
+    private static int createCheats(NodeGrid grid, int startX, int startY, int maxDistance) {
+        int picosecondsSaved = 100;
+        if (maxDistance == ROUNDS_PART_ONE && grid.getSize() <= GRID_SIZE_LIMIT_FOR_SAMPLES) {
+            picosecondsSaved = 10;
+        } else if (maxDistance == ROUNDS_PART_TWO && grid.getSize() <= GRID_SIZE_LIMIT_FOR_SAMPLES) {
+            //noinspection MagicNumber
+            picosecondsSaved = 50;
         }
-        return isSaving;
+        int savingCheats = 0;
+        for (int i = 1; i <= grid.getSize() - 2; i++) {
+            for (int j = 1; j <= grid.getSize() - 2; j++) {
+                if (grid.doesNodeExist(i, j) && gridDistance(startX, startY, i, j) <= maxDistance) {
+                    int newCostToHere = grid.getDistanceFromStartToHere(startX, startY) + gridDistance(startX, startY, i, j);
+                    if (newCostToHere + picosecondsSaved <= grid.getDistanceFromStartToHere(i, j)) {
+                        savingCheats++;
+                    }
+                }
+            }
+        }
+        return savingCheats;
     }
 
     /**
@@ -108,52 +128,19 @@ public class AoC202420 {
      */
     public static int partTwo(String path) throws FileNotFoundException {
         NodeGrid grid = readFile(path);
-
-        int shortestPath = grid.getDistanceFromStartToEnd();
-        int shorterPath = 0;
-
-        for (int i = 1; i <= grid.getSize() - 2; i++) {
-            for (int j = 1; j <= grid.getSize() - 2; j++) {
-                if (grid.doesNodeExist(i, j)) {
-                    shorterPath += createCheatsUpTo20(grid, i, j, shortestPath, path);
-                }
-            }
-        }
-
-        return shorterPath / 2;
+        grid.calculateDistances();
+        return calculateNumberOfCheats(grid, ROUNDS_PART_TWO);
     }
 
     /**
-     * Creates cheats up to 20 nodes away from the start node and checks if they save more than 50 or 100 picoseconds.
+     * Calculates the Manhattan distance between two points in a grid.
      *
-     * @param grid         the NodeGrid object representing the grid
-     * @param startX       the x-coordinate of the start node
-     * @param startY       the y-coordinate of the start node
-     * @param shortestPath the current shortest path length
-     * @param path         the path to the input file
-     * @return the number of saving cheats
-     * @throws FileNotFoundException if the file is not found
+     * @param x1 the x-coordinate of the first point
+     * @param y1 the y-coordinate of the first point
+     * @param x2 the x-coordinate of the second point
+     * @param y2 the y-coordinate of the second point
+     * @return the Manhattan distance between the two points
      */
-    private static int createCheatsUpTo20(NodeGrid grid, int startX, int startY, int shortestPath, String path)
-            throws FileNotFoundException {
-        int savingCheats = 0;
-        for (int i = 1; i <= grid.getSize() - 2; i++) {
-            for (int j = 1; j <= grid.getSize() - 2; j++) {
-                if (grid.doesNodeExist(i, j) && gridDistance(startX, startY, i, j) <= GRID_SIZE_LIMIT_FOR_SAMPLES) {
-                    NodeGrid sample = readFile(path);
-                    sample.addCrossEdge(startX, startY, i, j, gridDistance(startX, startY, i, j));
-                    int thisPath = sample.getDistanceFromStartToEnd();
-                    //noinspection MagicNumber
-                    int picosecondsSaved = grid.getSize() <= GRID_SIZE_LIMIT_FOR_SAMPLES ? 50 : 100;
-                    if (thisPath <= shortestPath - picosecondsSaved) {
-                        savingCheats++;
-                    }
-                }
-            }
-        }
-        return savingCheats;
-    }
-
     private static int gridDistance(int x1, int y1, int x2, int y2) {
         return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
